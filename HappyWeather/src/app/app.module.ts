2974604b258c398742085b5bf022ed63f61e5a8a
Antoni Danov <antoni.danov@gmail.com>
2023-08-24 22:17:16 +0200

    Add angular metreial modules

--- HappyWeather/src/app/app.module.ts
+++ HappyWeather/src/app/app.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card'
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 
@@ -12,7 +16,11 @@ import { HomeComponent } from './components/home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    MatInputModule,
+    MatIconModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
