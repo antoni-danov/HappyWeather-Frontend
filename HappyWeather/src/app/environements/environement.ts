6bea4bcc5fc3ec2f9f55d6a9a93c361a1995b691
Antoni <59748393+antoni-danov@users.noreply.github.com>
2023-09-26 19:30:59 +0200

    Delete HappyWeather/src/app/environements directory

--- HappyWeather/src/app/environements/environement.ts
+++
@@ -1,7 +0,0 @@
-export const environement = {
-    production: false,
-    localhost: 'https://localhost:7087/api/weather',
-    googleMapsApiKey: 'AIzaSyA1e22ZRbewpjj3PFaRADIRDPMcLCs8iCY',
-    locationSearch: 'https://www.google.com/search?q=',
-    jsonIconsList: '../assets/iconsList.json'
-};
