37cd6d72ebfd4216d1f294cd68ae3c4abcd5aadd
Antoni Danov <antoni.danov@gmail.com>
2023-10-05 10:21:03 +0200

    Visual changes on five days forecast

--- HappyWeather/src/app/components/five-days-forecast/five-days-forecast.component.ts
+++ HappyWeather/src/app/components/five-days-forecast/five-days-forecast.component.ts
@@ -1,6 +1,6 @@
+import { CurrencyPipe, DatePipe } from '@angular/common';
 import { AfterViewInit, ChangeDetectorRef, Component } from '@angular/core';
 import { DailyWeatherForecast } from 'src/app/interfaces/DailyForecast/dailyWeatherForecast';
-import { DayUnit } from 'src/app/interfaces/DailyForecast/dayUnit';
 import { WeatherService } from 'src/app/services/weatherService/weather.service';
 
 @Component({
@@ -11,8 +11,10 @@ import { WeatherService } from 'src/app/services/weatherService/weather.service'
 export class FiveDaysForecastComponent implements AfterViewInit {
 
   fivedaysForecast!: DailyWeatherForecast;
+  date: any;
 
-  constructor(private service: WeatherService, private cdref: ChangeDetectorRef) {
+  constructor(private service: WeatherService, private cdref: ChangeDetectorRef,
+    private datePipe: DatePipe) {
 
   }
   ngAfterViewInit() {
@@ -21,6 +23,15 @@ export class FiveDaysForecastComponent implements AfterViewInit {
   getFiveDaysWeatherForecas() {
     this.service.fiveDaysData$.subscribe(data => {
       this.fivedaysForecast = data;
+      this.date = this.fivedaysForecast.timeLines.daily.map(day => {
+        const curentDay = this.datePipe.transform(day.time.split('T')[0], 'EEEE, dd MMMM');
+        return {
+          day: curentDay
+        }
+      });
+      console.log(this.date);
+
+      // this.date = this.datePipe.transform(this.fivedaysForecast.timeLines.daily.time.split('T')[0], 'EEEE, dd MMMM');
       console.log(this.fivedaysForecast);
 
     });
